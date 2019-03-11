4967a41fe8e68c37833cea78ca2f2666b75cffb9
Even Solbraa <esol@equinor.com>
2019-03-11 16:52:57 +0100

    updates to fix bug with colon/semicolon on linux

--- src/neqsim/javaGateway.py
+++ src/neqsim/javaGateway.py
@@ -2,8 +2,15 @@ import os
 from os import path
 import pkg_resources
 import subprocess
+import shlex
+import os
 
 def startServer():
+    localosname = os.name
+    colon = ':'
+    if(localosname == 'nt'):
+        colon=';'
     resources_dir = pkg_resources.resource_filename('neqsim', 'lib/')
-    fullstring = 'java -classpath '+resources_dir+'/NeqSimpy4j.jar;'+resources_dir+'/NeqSimSource.jar;'+resources_dir+'/py4j0.10.8.1.jar neqsimpy4j.startJavaGateway'
+    #resources_dir = "/usr/local/lib/python3.6/dist-packages/neqsim/lib"
+    fullstring = shlex.split('java -classpath '+resources_dir+'/NeqSimpy4j.jar'+colon+resources_dir+'/NeqSimSource.jar'+colon+resources_dir+'/py4j0.10.8.1.jar neqsimpy4j.startJavaGateway')
     pipe = subprocess.Popen(fullstring,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
