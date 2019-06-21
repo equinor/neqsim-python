892453ef51abc021e143da99f59183a9ed45c6ca
Even Solbraa <esol@equinor.com>
2019-06-21 12:26:39 +0200

    fixed indent issue in init

--- src/neqsim/__init__.py
+++ src/neqsim/__init__.py
@@ -4,6 +4,6 @@ java_gateway = javaGateway.start_server()
 
 def methods(checkClass):
     methods = checkClass.getClass().getMethods()
-        for method in methods:
-            print(method.getName())
+    for method in methods:
+        print(method.getName())
             
