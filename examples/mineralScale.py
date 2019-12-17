64eea83142fe322b19227c80ad4643b3a67f7b27
Even Solbraa <esol@equinor.com>
2019-12-17 13:00:17 +0100

    updates to compressor calculations

---
+++ examples/mineralScale.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 11 21:19:21 2019
+
+@author: esol
+"""
+
+
+from neqsim.thermo import fluid,printFrame, TPflash, checkScalePotential, calcIonComposition, printFluid, table
+nitrogen = 1.0 #@param {type:"number"}
+CO2 = 1.1 #@param {type:"number"}
+methane = 88.3  #@param {type:"number"}
+ethane = 5.0  #@param {type:"number"}
+propane =  1.5 #@param {type:"number"}
+water =  1.25 #@param {type:"number"}
+Naplus =  0.025 #@param {type:"number"}
+Clminus =  0.025 #@param {type:"number"}
+Caplus =  0.00025 #@param {type:"number"}
+Feplus =  0.00025 #@param {type:"number"}
+OHminus =  0.00025 #@param {type:"number"}
+
+fluid1 = fluid('Electrolyte-CPA-EoS')
+fluid1.addComponent("nitrogen", nitrogen)
+fluid1.addComponent("CO2", CO2)
+fluid1.addComponent("methane", methane)
+fluid1.addComponent("ethane", ethane)
+fluid1.addComponent("propane", propane)
+fluid1.addComponent("water", water,"kg/sec")
+fluid1.addComponent("Na+", Naplus,"mol/sec")
+fluid1.addComponent("Cl-", Clminus,"mol/sec")
+fluid1.addComponent("Ca++", Caplus,"mol/sec")
+fluid1.addComponent("Fe++", Feplus,"mol/sec")
+fluid1.addComponent("OH-", OHminus,"mol/sec")
+fluid1.chemicalReactionInit()
+fluid1.setMixingRule(10)
+
+
+TPflash(fluid1)
+ionCompResults = calcIonComposition(fluid1)
+scaleResults = checkScalePotential(fluid1)
+
+print("pH of water ",fluid1.getPhase("aqueous").getpH())
+
+import pandas
+printFrame(fluid1)
+print(pandas.DataFrame(ionCompResults))
+print(pandas.DataFrame(scaleResults))
--- setup.py
+++ setup.py
@@ -5,16 +5,23 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="neqsim",
-    version="1.1.0",
+    version="1.2.5",
     author="Even Solbraa",
     author_email="esolbraa@gmail.com",
     description="NeqSim is a tool for thermodynamic and process calculations",
-    long_description="NeqSim is a tool for thermodynamic and process calculations",
+    long_description="NeqSim (Non-Equilibrium Simulator) is a library for estimation of fluid behaviour for oil and gas production. The basis for NeqSim is fundamental mathematical models related to phase behaviour and physical properties of oil and gas.",
     long_description_content_type="text/markdown",
     url="https://github.com/Equinor/neqsimpython",
+    #packages=setuptools.find_packages(),
     packages=['neqsim', "neqsim.thermo", "neqsim.process", "neqsim.standards"],
     package_dir={'neqsim': 'src/neqsim'},
     package_data={'neqsim': ['lib/*.jar']},
     include_package_data=True,
-    install_requires=['py4j']
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['py4j'],
+    python_requires='>=3'
 )
