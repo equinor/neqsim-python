105c59a45245a3cb3ee20b2b25a55ab89d3e3a48
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2019-12-20 14:45:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="neqsim",
-    version="1.2.10",
+    version="1.2.11",
     author="Even Solbraa",
     author_email="esolbraa@gmail.com",
     description="NeqSim is a tool for thermodynamic and process calculations",
