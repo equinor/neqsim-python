40e2fc51e11c0b28c24a93c9d086dae7f0becda1
Even Solbraa <esol@equinor.com>
2020-05-28 10:41:18 +0200

    updated mercury models and parameters

--- src/neqsim/thermo/thermoTools.py
+++ src/neqsim/thermo/thermoTools.py
@@ -37,6 +37,7 @@ fluid_type = {
     'srk-twoCoon': neqsim.thermo.system.SystemSrkTwuCoonParamEos,
     'cpa-pr': neqsim.thermo.system.SystemPrCPA,
     'CPA-PR-EoS': neqsim.thermo.system.SystemPrCPA,
+    'SRK-TwuCoon-EOS': neqsim.thermo.system.SystemSrkTwuCoonEos
 }
 
 
