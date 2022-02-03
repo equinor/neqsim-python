1e4a91dde1f27239eab86fdbdfb7701d03c9c738
EvenSol <esolbraa@gmail.com>
2022-02-03 18:38:10 +0100

    updates

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="neqsim",
-    version="2.3.13",
+    version="2.3.14",
     author="Even Solbraa",
     author_email="esolbraa@gmail.com",
     description="NeqSim is a tool for thermodynamic and process calculations",
