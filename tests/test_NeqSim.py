8e92362cd4b93578f6556330b1c35f1671876a47
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2022-03-09 22:41:10 +0100

    wip

---
+++ tests/test_NeqSim.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  3 22:24:08 2019
+
+@author: ESOL
+"""
+from neqsim.neqsimpython import neqsim
+
+
+def test_viscosity():
+    thermoSystem = neqsim.thermo.system.SystemSrkEos(280.0, 10.0)
+    thermoSystem.addComponent("methane", 10.0)
+    thermoSystem.addComponent("water", 4.0)
+
+    thermoOps = neqsim.thermodynamicOperations.ThermodynamicOperations(
+        thermoSystem)
+    thermoOps.TPflash()
+
+    gasEnthalpy = thermoSystem.getPhase(0).getEnthalpy()
+
+    thermoSystem.initPhysicalProperties("Viscosity")
+    gasViscosity = thermoSystem.getPhase(0).getViscosity("kg/msec")
+
+    print("viscosity", gasViscosity)
