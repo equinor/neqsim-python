3b22b966c1f3c0e4901e1c0ff8f8d0d2b4e205b2
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2022-05-15 13:37:20 +0000

    test

--- neqsim/neqsimpython.py
+++ neqsim/neqsimpython.py
@@ -2,6 +2,9 @@ import jpype
 
 if not(jpype.isJVMStarted()):
     jpype.addClassPath('./lib/*')
-    jpype.startJVM(convertStrings=True)
-
+    try:
+        jpype.startJVM(convertStrings=True)
+    except:
+        print('could not start JVM')
+        
 jNeqSim = jpype.JPackage('neqsim')
