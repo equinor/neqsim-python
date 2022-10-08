79f3cc6bd7ee83ca92e0c6cd000c1ca8fb1e99a3
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2022-10-08 06:01:12 +0000

    use neqsim java8 library if java8 is installed

--- neqsim/neqsimpython.py
+++ neqsim/neqsimpython.py
@@ -1,10 +1,14 @@
 import jpype
 
 if not(jpype.isJVMStarted()):
-    jpype.addClassPath('./lib/*')
+    #jpype.addClassPath('./lib/*')
     try:
         jpype.startJVM(convertStrings=False)
+        jvmVersion = jpype.getJVMVersion()[0]
     except:
         print('could not start JVM from jpype')
-        
+    if jvmVersion <= 8:
+        jpype.addClassPath('./lib/libj8/*')  
+    else:
+        jpype.addClassPath('./lib/*') 
 jNeqSim = jpype.JPackage('neqsim')
