86ad73ee4b241441c366c7cd1bf1b5a09b316f13
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2022-10-31 09:33:05 +0100

    fixed bug odd oil fractions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="neqsim",
-    version="2.4.4",
+    version="2.4.5",
     author="Even Solbraa",
     author_email="esolbraa@gmail.com",
     description="NeqSim is a tool for thermodynamic and process calculations",
