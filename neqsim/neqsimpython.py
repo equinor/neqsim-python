ed25b455cef23bdb1b23c92190048548c2729450
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2023-03-31 15:20:41 +0000

    update lib

--- neqsim/neqsimpython.py
+++ neqsim/neqsimpython.py
@@ -2,10 +2,7 @@ import jpype
 
 if not jpype.isJVMStarted():
     jvmVersion = jpype.getJVMVersion()[0]
-    if jvmVersion <= 8:
-        jpype.addClassPath('./lib/libj8/*')
-    else:
-        jpype.addClassPath('./lib/*')
+    jpype.addClassPath('./lib/*')
     jpype.startJVM(convertStrings=False)
 
 jNeqSim = jpype.JPackage('neqsim')
