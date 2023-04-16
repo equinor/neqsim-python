873f319f7e884f3f4154542100c18c85999d1112
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2023-04-16 07:38:24 +0000

    update check version of java

--- neqsim/neqsimpython.py
+++ neqsim/neqsimpython.py
@@ -2,9 +2,10 @@ import jpype
 
 if not jpype.isJVMStarted():
     jvmVersion = jpype.getJVMVersion()[0]
-    if jvmVersion <= 8:
-        jpype.addClassPath('./lib/*')
-        #jpype.addClassPath('./lib/libj8/*')
+    if jvmVersion<1:
+        print('could not detech JVM version. Using version Java 11+ of neqsim library.')
+    if jvmVersion <= 8 and jvmVersion>1:
+        jpype.addClassPath('./lib/libj8/*')
     else:
         jpype.addClassPath('./lib/*')
     jpype.startJVM(convertStrings=False)
