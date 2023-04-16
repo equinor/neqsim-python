f2efb81c216b17e600c0bc57dccd196e41aec7e0
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2023-04-16 09:30:39 +0000

    require version 11 or higher for Java

--- neqsim/neqsimpython.py
+++ neqsim/neqsimpython.py
@@ -4,5 +4,8 @@ if not jpype.isJVMStarted():
     jpype.addClassPath('./lib/*')
     jpype.startJVM(convertStrings=False)
     jvmVersion = jpype.getJVMVersion()[0]
-    
+    if jvmVersion<=8:
+        print('Your version of Java is not supproted. Please upgrade to Java version 11 or higher.')
+        print('See: https://github.com/equinor/neqsimpython#prerequisites')
+
 jNeqSim = jpype.JPackage('neqsim')
