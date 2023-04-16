0df2c50c435d090285460a095ae84e9ae3f3d0c7
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2023-04-16 07:12:50 +0000

    removed library

--- neqsim/neqsimpython.py
+++ neqsim/neqsimpython.py
@@ -3,7 +3,8 @@ import jpype
 if not jpype.isJVMStarted():
     jvmVersion = jpype.getJVMVersion()[0]
     if jvmVersion <= 8:
-        jpype.addClassPath('./lib/libj8/*')
+        jpype.addClassPath('./lib/*')
+        #jpype.addClassPath('./lib/libj8/*')
     else:
         jpype.addClassPath('./lib/*')
     jpype.startJVM(convertStrings=False)
