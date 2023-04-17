b76614f9d6c8cc123eee49407d2d0ea4f11c4d3c
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2023-04-17 14:10:14 +0000

    fixed text error

--- neqsim/neqsimpython.py
+++ neqsim/neqsimpython.py
@@ -5,7 +5,7 @@ if not jpype.isJVMStarted():
     jpype.startJVM(convertStrings=False)
     jvmVersion = jpype.getJVMVersion()[0]
     if jvmVersion<=8:
-        print('Your version of Java is not supproted. Please upgrade to Java version 11 or higher.')
+        print('Your version of Java is not supported. Please upgrade to Java version 11 or higher.')
         print('See: https://github.com/equinor/neqsimpython#prerequisites')
 
 jNeqSim = jpype.JPackage('neqsim')
