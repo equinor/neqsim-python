fb2583a28e6c0f3809f2b832d82204fa235e4024
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2023-06-05 22:36:58 +0200

    version 2.5.1
    update with neqsim library version 2.5.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ interactive_req = ['matplotlib', 'tabulate', 'jupyter']
 
 setup(
     name="neqsim",
-    version="2.5.0",
+    version="2.5.1",
     author="Even Solbraa",
     author_email="esolbraa@gmail.com",
     description="NeqSim is a tool for thermodynamic and process calculations",
