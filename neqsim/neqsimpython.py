30edc3fb27d2103c87a1f58c51072cd94293af08
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2024-01-08 16:00:23 +0100

    feat: support java 8, 11 and 21 (#224)
    * feat: support java 8 in addition to java 11+

--- neqsim/neqsimpython.py
+++ neqsim/neqsimpython.py
@@ -1,12 +1,19 @@
 import jpype
 
 if not jpype.isJVMStarted():
-    jpype.addClassPath("./lib/*")
+    # Could call jpype.getDefaultJVMPath() to get default JVM,
+    # but not able to get the orders to force loading a specific JVM
     jpype.startJVM(convertStrings=False)
-    jvmVersion = jpype.getJVMVersion()[0]
-    if jvmVersion <= 8:
+    jvm_version = jpype.getJVMVersion()[0]
+    if jvm_version == 1 and jpype.getJVMVersion()[1] >= 8:
+        jpype.addClassPath("./lib/java8/*")
+    # elif jvm_version >= 21:
+    #    jpype.addClassPath("./lib/java21/*")
+    elif jvm_version >= 11:
+        jpype.addClassPath("./lib/java11/*")
+    else:
         print(
-            "Your version of Java is not supported. Please upgrade to Java version 11 or higher."
+            "Your version of Java is not supported. Please upgrade to Java version 8 or higher."
         )
         print("See: https://github.com/equinor/neqsimpython#prerequisites")
 
