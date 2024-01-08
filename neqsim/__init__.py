2c871ef0ce607f19cd12e1759cfde515499b66ff
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2024-01-08 15:31:46 +0100

    test

--- neqsim/__init__.py
+++ neqsim/__init__.py
@@ -26,15 +26,17 @@ def has_tabulate():
 
 
 def setDatabase(connectionString):
-    jNeqSim.util.database.NeqSimDataBase.setConnectionString(connectionString)
+    jNeqSim.util.database.NeqSimDataBase.setConnectionString(connectionString)  
+    
+    
     jNeqSim.util.database.NeqSimDataBase.setCreateTemporaryTables(True)
 
 
 def save_xml(javaobject, filename):
-    xstream = jpype.JPackage("com.thoughtworks.xstream")
-    streamer = xstream.XStream()
-    xml = streamer.toXML(javaobject)
-    print(xml, file=open(filename, "w"))
+    xstream = jpype.JPackage('com.thoughtworks.xstream')
+    streamer=xstream.XStream()
+    xml = streamer.toXML(javaobject )
+    print(xml, file=open(filename, 'w') )
     return xml
 
 
