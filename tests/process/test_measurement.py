fdc5c1d53b346b824fc729fb5ecfc73a5ec646e2
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2024-08-05 08:18:49 +0000

    add measurement

---
+++ tests/process/test_measurement.py
@@ -0,0 +1,47 @@
+from neqsim.process.measurement import measurement
+from neqsim.process.processTools import (
+    pump,
+    stream,
+    clearProcess,
+    runProcess,
+    pumpChart,
+)
+from neqsim.thermo import fluid
+from neqsim import jNeqSim
+from jpype import JImplements, JOverride
+
+class ExampleMeasurement(measurement):
+    def __init__(self):
+        super().__init__()
+        self.name = ""
+    
+    def setInputStream(self, stream):
+        self.inputstream = stream
+        self.outputstream = stream.clone()
+
+    def getMeasuredValue(self):
+        return self.inputstream.getPressure("psia")
+    
+
+def test_addPythonUnitOp():
+    fluid1 = fluid("srk")  # create a fluid using the SRK-EoS
+    fluid1.setTemperature(30.0, "C")
+    fluid1.setPressure(1.0, "bara")
+    fluid1.addComponent("n-pentane", 1.0, "kg/sec")
+    fluid1.addComponent("n-hexane", 1.0, "kg/sec")
+    fluid1.setMixingRule(2)
+
+    stream1 = jNeqSim.processSimulation.processEquipment.stream.Stream(fluid1)
+    stream1.setFlowRate(30000, "kg/hr")
+    
+    meas1 = ExampleMeasurement()
+    meas1.setName('example measurement 1')
+    meas1.setInputStream(stream1)
+
+    oilprocess = jNeqSim.processSimulation.processSystem.ProcessSystem()
+    oilprocess.add(stream1)
+    oilprocess.add(meas1)
+    oilprocess.run()
+
+    assert stream1.getPressure("psia")==meas1.getMeasuredValue()
+
