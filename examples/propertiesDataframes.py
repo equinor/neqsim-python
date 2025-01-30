38f6d55ad43aa064411e9fe2075c166eae7ffca6
Åsmund Våge Fannemel <ASMF@equinor.com>
2025-01-30 10:27:16 +0100

    chore: remove warnings

--- examples/propertiesDataframes.py
+++ examples/propertiesDataframes.py
@@ -11,10 +11,7 @@ import pandas as pd
 from neqsim.thermo import (
     TPflash,
     calcproperties,
-    fluid,
     fluid_df,
-    phaseenvelope,
-    printFrame,
 )
 
 # create gas condensate fluid
@@ -149,9 +146,9 @@ def calcProperties(frame):
         frame["gasmolfraction[mol/mol]"] = gascondensateFluid.getMoleFraction(
             phaseNumber
         )
-        frame[
-            "gasvolumefraction[mol/mol]"
-        ] = gascondensateFluid.getCorrectedVolumeFraction(phaseNumber)
+        frame["gasvolumefraction[mol/mol]"] = (
+            gascondensateFluid.getCorrectedVolumeFraction(phaseNumber)
+        )
         frame["gasthermalconductivity[W/mK]"] = gascondensateFluid.getPhase(
             phaseNumber
         ).getThermalConductivity("W/mK")
@@ -167,9 +164,9 @@ def calcProperties(frame):
         frame["oilmolfraction[mol/mol]"] = gascondensateFluid.getMoleFraction(
             phaseNumber
         )
-        frame[
-            "oilvolumefraction[mol/mol]"
-        ] = gascondensateFluid.getCorrectedVolumeFraction(phaseNumber)
+        frame["oilvolumefraction[mol/mol]"] = (
+            gascondensateFluid.getCorrectedVolumeFraction(phaseNumber)
+        )
         frame["oilthermalconductivity[W/mK]"] = gascondensateFluid.getPhase(
             phaseNumber
         ).getThermalConductivity("W/mK")
@@ -182,9 +179,9 @@ def calcProperties(frame):
         frame["oilmolfraction[mol/mol]"] = gascondensateFluid.getMoleFraction(
             phaseNumber
         )
-        frame[
-            "oilvolumefraction[mol/mol]"
-        ] = gascondensateFluid.getCorrectedVolumeFraction(phaseNumber)
+        frame["oilvolumefraction[mol/mol]"] = (
+            gascondensateFluid.getCorrectedVolumeFraction(phaseNumber)
+        )
         frame["oilthermalconductivity[W/mK]"] = gascondensateFluid.getPhase(
             phaseNumber
         ).getThermalConductivity("W/mK")
