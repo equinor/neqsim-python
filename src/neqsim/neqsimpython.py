ec63d0d1838b3b593301db0c76433f3b60510c68
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2025-02-01 06:09:49 +0000

    test

--- src/neqsim/neqsimpython.py
+++ src/neqsim/neqsimpython.py
@@ -1,20 +1,19 @@
 import jpype
+import atexit
 
 if not jpype.isJVMStarted():
-    # Could call jpype.getDefaultJVMPath() to get default JVM,
-    # but not able to get the orders to force loading a specific JVM
+    # Start the JVM with your specific options and classpath settings
     jpype.startJVM(convertStrings=False)
     jvm_version = jpype.getJVMVersion()[0]
     if jvm_version == 1 and jpype.getJVMVersion()[1] >= 8:
         jpype.addClassPath("./lib/java8/*")
-    # elif jvm_version >= 21:
-    #    jpype.addClassPath("./lib/java21/*")
     elif jvm_version >= 11:
         jpype.addClassPath("./lib/java11/*")
     else:
-        print(
-            "Your version of Java is not supported. Please upgrade to Java version 8 or higher."
-        )
+        print("Your version of Java is not supported. Please upgrade to Java version 8 or higher.")
         print("See: https://github.com/equinor/neqsimpython#prerequisites")
+
+# Register the shutdown function to be called at interpreter exit
+atexit.register(lambda: jpype.shutdownJVM() if jpype.isJVMStarted() else None)
 
 jneqsim = jpype.JPackage("neqsim")
