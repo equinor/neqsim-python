c1d5f7ad61d5462e193353cce76e243bec76cbfc
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2025-02-01 06:16:21 +0000

    upda

--- tests/process/test_unitop.py
+++ tests/process/test_unitop.py
@@ -34,7 +34,7 @@ class ExampleCompressor(unitop):
         self.outputstream.run()
 
 
-def test_addPythonUnitOp():
+def aatest_addPythonUnitOp():
     fluid1 = fluid("srk")  # create a fluid using the SRK-EoS
     fluid1.setTemperature(30.0, "C")
     fluid1.setPressure(1.0, "bara")
